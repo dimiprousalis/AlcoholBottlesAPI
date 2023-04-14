6d148c08d85f24a4cd7744d997ac68e62095bb65
Dimitra Prousalis <dimitra.o.p@gmail.com>
2023-04-14 09:48:06 -0700

    remove catch

--- server.js
+++ server.js
@@ -12,43 +12,43 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
-
-
-
-
-        app.use(express.urlencoded({ extended: true }))
-        app.use(express.json())
-
-        app.get('/', (request, response) => {
-            response.sendFile(__dirname + '/index.html')
-        })
-
-
-        //Return entire DB
-        app.get('/api', (request, response) => {
-            //go to the database and find the collection and put all objects into an array
-            db.collection('spirits').find().toArray()
-                .then(data => {
-                    response.json(data)
-                })
-                .catch(error => console.error(error))
-        })
-
-        //Search for specific bottle type
-        app.get('/api/:name', (request, response) => {
-            const bottleType = request.params.name.toUpperCase()
-            let query = { Type: bottleType }
-            db.collection('spirits').find(query).toArray()
-                .then(data => {
-                    response.json(data)
-                })
-                .catch(error => console.error(error))
-        })
-
-        //adding process.env.PORT allows use on hosting port
-        app.listen(process.env.PORT || PORT, () => {
-            console.log(`The server is now running on ${PORT}! Better go catch it!`)
-        })
-
     })
-    .catch(console.error)
+
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+
+//Return entire DB
+app.get('/api', (request, response) => {
+    //go to the database and find the collection and put all objects into an array
+    db.collection('spirits').find().toArray()
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => console.error(error))
+})
+
+//Search for specific bottle type
+app.get('/api/:name', (request, response) => {
+    const bottleType = request.params.name.toUpperCase()
+    let query = { Type: bottleType }
+    db.collection('spirits').find(query).toArray()
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => console.error(error))
+})
+
+//adding process.env.PORT allows use on hosting port
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is now running on ${PORT}! Better go catch it!`)
+})
+
+
+
